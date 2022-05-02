b555932d5f379f41e5f5235c3466d29d403afd1f
Phil Wright <touchopia@me.com>
2022-05-02 12:15:51 -0600

    Adding APIClient singleton

---
+++ MealDB-MVC/APIClient.swift
@@ -0,0 +1,17 @@
+//
+//  APIClient.swift
+//  MealDB-MVC
+//
+//  Created by Phil Wright on 5/2/22.
+//
+
+import Foundation
+
+class APIClient {
+    static let shared = APIClient()
+    private init() {}
+    
+    func getMeals() {
+        
+    }
+}
