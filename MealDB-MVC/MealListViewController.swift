7b6c7614144828443f91728f418532f242e239b7
Phil Wright <touchopia@me.com>
2022-05-02 12:13:45 -0600

    Adding MealTableView custom cell + Assets

--- MealDB-MVC/MealListViewController.swift
+++ MealDB-MVC/MealListViewController.swift
@@ -9,10 +9,53 @@ import UIKit
 
 class MealListViewController: UIViewController {
 
+    private let cellIdentifier = "MealTableViewCell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Meals"
+        
+        setupTableView()
     }
+    
+    func setupTableView() {
+        tableView.register(UINib(nibName: "MealTableViewCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
+    }
+    
+}
+
+extension MealListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 88
+    }
+    
+}
+
+extension MealListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? MealTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.mealImageView.image = UIImage(named: "placeholder-image")
+        cell.titleLabel.text = "Testing"
+        return cell
+    }
+    
     
 }
 
---
+++ MealDB-MVC/MealTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MealTableViewCell.swift
+//  themealdb
+//
+//  Created by Phil Wright on 4/27/22.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var mealImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
