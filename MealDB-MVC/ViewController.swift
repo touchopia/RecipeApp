5ea5ade229546b80c7911a8384dc04bfda2652a3
Phil Wright <touchopia@me.com>
2022-05-02 11:26:34 -0600

    initial commit

---
+++ MealDB-MVC/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MealDB-MVC
+//
+//  Created by Phil Wright on 5/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
---
+++ MealDB-MVCTests/MealDB_MVCTests.swift
@@ -0,0 +1,13 @@
+//
+//  MealDB_MVCTests.swift
+//  MealDB-MVCTests
+//
+//  Created by Phil Wright on 5/2/22.
+//
+
+import XCTest
+@testable import MealDB_MVC
+
+class MealDB_MVCTests: XCTestCase {
+
+}
