c61b1cb203a75c449ac7e61d933f17bed084d24b
Phil Wright <touchopia@me.com>
2022-06-17 11:42:05 -0600

    Adding Unit Testing and failing test.

---
+++ final/RecipeApp/RecipeAppTests/RecipeAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeAppTests.swift
+//  RecipeAppTests
+//
+//  Created by Phil Wright on 6/17/22.
+//
+
+import XCTest
+@testable import RecipeApp
+
+class RecipeAppTests: XCTestCase {
+
+    func test_failingTest() {
+        XCTAssert(false)
+    }
+}
